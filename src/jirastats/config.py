c2501d4f4a52ff34fa05fe707b74002b6608192b
Yusra AlSayyad <yusra@astro.princeton.edu>
2024-05-08 20:41:03 +0000

    Initial commit

---
+++ src/jirastats/config.py
@@ -0,0 +1,35 @@
+"""Configuration definition."""
+
+from __future__ import annotations
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from safir.logging import LogLevel, Profile
+
+__all__ = ["Config", "config"]
+
+
+class Config(BaseSettings):
+    """Configuration for jira_stats."""
+
+    name: str = Field("jira_stats", title="Name of application")
+
+    path_prefix: str = Field(
+        "/jira_stats", title="URL prefix for application"
+    )
+
+    profile: Profile = Field(
+        Profile.development, title="Application logging profile"
+    )
+
+    log_level: LogLevel = Field(
+        LogLevel.INFO, title="Log level of the application's logger"
+    )
+
+    model_config = SettingsConfigDict(
+        env_prefix="JIRA_STATS_", case_sensitive=False
+    )
+
+
+config = Config()
+"""Configuration for jira_stats."""
